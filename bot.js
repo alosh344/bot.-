e8f43f3e744f2de7fe4044d4fd882128f3e2d54f
alosh344 <62521679+alosh344@users.noreply.github.com>
2020-04-06 14:59:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,5 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-client.login("NjE2NzE0MjIxNDUwNjkwNTY5.XosORg.FRYMR0ho_fmYUnP8Bzfb7swyqJg"); 
-
 client.on('message', message => {
     if(message.content === 'هلو'){
         message.channel.send('الرد')
